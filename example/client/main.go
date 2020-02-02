633e4e8db358e712ab876fd5f38f8bfb1207f20a
Henrique Vicente <henriquevicente@gmail.com>
2020-02-02 16:56:53 +0100

    example: adding code for usage on client-side and server-side.

---
+++ example/client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/henvic/httpretty"
+)
+
+func main() {
+	logger := &httpretty.Logger{
+		Time:           true,
+		TLS:            true,
+		RequestHeader:  true,
+		RequestBody:    true,
+		ResponseHeader: true,
+		ResponseBody:   true,
+		Colors:         true, // erase line if you don't like colors
+		Formatters:     []httpretty.Formatter{&httpretty.JSONFormatter{}},
+	}
+
+	client := &http.Client{
+		Transport: logger.RoundTripper(http.DefaultTransport),
+	}
+
+	if _, err := client.Get("https://www.google.com/"); err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ example/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/henvic/httpretty"
+)
+
+func main() {
+	logger := &httpretty.Logger{
+		Time:           true,
+		TLS:            true,
+		RequestHeader:  true,
+		RequestBody:    true,
+		ResponseHeader: true,
+		ResponseBody:   true,
+		Colors:         true, // erase line if you don't like colors
+	}
+
+	addr := ":8090"
+	fmt.Printf("Open http://localhost%s in the browser.\n", addr)
+
+	if err := http.ListenAndServe(":8090", logger.Middleware(helloHandler{})); err != http.ErrServerClosed {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+type helloHandler struct{}
+
+func (h helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header()["Date"] = nil
+	fmt.Fprintf(w, "Hello, world!")
+}
