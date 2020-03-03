henvic/httpretty

commit c3069c07cb162c7edf7fb3383e7467ed6913cabd

ci: updating supported Go versions to latest 1.14 and 1.13.

.travis.yml
@@ -3,7 +3,8 @@ sudo: false
 language: go
 
 go:
-  - "1.13.8"
+  - "1.14"
+  - "1.13"
   - "tip"
 
 matrix:

commit 131bcdb5ea8e9d70129dbe69773392b4509d7ee8

ci: fixing and improving testing environment.
Fix Go version issue introduced in commit c3069c07cb162c7edf7fb3383e7467ed6913cabd.
Adding CI jobs for macOS and Windows.
Skipping static code analysis on all CI jobs but a main one to speed up testing.

.travis.yml
@@ -1,28 +1,39 @@
-sudo: false
-
 language: go
 
 go:
-  - "1.14"
-  - "1.13"
+  - "1.14.x" # when updating to a new major release, update scripts below too
+  - "1.13.x"
   - "tip"
 
-matrix:
+os:
+  - linux
+  - osx
+  - windows
+
+jobs:
   allow_failures:
     - go: tip
+    - os: windows
 
 env:
   - GO111MODULE=on
 
 install:
-  - go get github.com/mattn/goveralls
-  - go get golang.org/x/lint/golint
-  - go get honnef.co/go/tools/cmd/staticcheck
-  - go get github.com/securego/gosec/cmd/gosec
-  - go get golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
+  - |
+    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_GO_VERSION" == "1.14.x" ]; then
+      ./scripts/install-ci.sh;
+    fi
 
 script:
-  - ./scripts/test.sh
+  - |
+    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_GO_VERSION" == "1.14.x" ]; then
+      ./scripts/test.sh;
+    else
+      go test ./... -race
+    fi
 
 after_success:
-  - ./scripts/coverage.sh --coveralls
+  - |
+    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_GO_VERSION" == "1.14.x" ]; then
+      ./scripts/coverage.sh --coveralls;
+    fi

scripts/coverage.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Modified version of chef-runner/script/coverage
 # Copyright 2004 Mathias Lafeldt

scripts/install-ci.sh
@@ -1,0 +1,10 @@
+#!/bin/bash
+set -euox pipefail
+
+# TODO(henvic): install specific versions of the commands
+# when the 3 latest releases of the Go toolchains supports it using @tag.
+go install github.com/mattn/goveralls
+go install golang.org/x/lint/golint
+go install honnef.co/go/tools/cmd/staticcheck
+go install github.com/securego/gosec/cmd/gosec
+go install golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow